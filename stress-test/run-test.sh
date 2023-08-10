c30deea05360b09f1d574442b60319f2dd40df36
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2023-08-10 00:58:02 -0300

    update zanfranceschi participação

--- stress-test/run-test.sh
+++ stress-test/run-test.sh
@@ -1,5 +1,5 @@
 # Exemplos de requests
-# curl -v -XPOST -H "content-type: application/json" -d '{"apelido" : "xpto", "nome" : "xpto xpto", "nascimento" : "2000-01-01", stack: null}' "http://localhost:9999/pessoas"
+# curl -v -XPOST -H "content-type: application/json" -d '{"apelido" : "xpto", "nome" : "xpto xpto", "nascimento" : "2000-01-01", "stack": null}' "http://localhost:9999/pessoas"
 # curl -v -XGET "http://localhost:9999/pessoas/1"
 # curl -v -XGET "http://localhost:9999/pessoas?t=xpto"
 # curl -v "http://localhost:9999/contagem-pessoas"
