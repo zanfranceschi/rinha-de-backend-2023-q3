123dc0598b402a9892b947f9b58985a0a6370966
Luiz Picanço <lpicanco@gmail.com>
2023-08-20 12:22:01 +0100

    add lpicanco(v1)

---
+++ participantes/lpicanco/schema.sql
@@ -0,0 +1,13 @@
+\c dogfight
+
+CREATE TABLE pessoas
+(
+    id         uuid PRIMARY KEY,
+    apelido    VARCHAR(32)  NOT NULL,
+    nome       VARCHAR(100) NOT NULL,
+    nascimento DATE  NOT NULL,
+    stack      VARCHAR(32)[],
+    search_vector text
+);
+
+CREATE INDEX idx_search_vector_pessoas ON pessoas(search_vector)
