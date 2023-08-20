f33854e8c2a8b493c794e56539d4a494a3c65023
Vinicius Santos <viniciusmsos@gmail.com>
2023-08-20 18:54:19 -0300

    update migration

--- participantes/vimsos/config/migrations/01_person_table.sql
+++ participantes/vimsos/config/migrations/01_person_table.sql
@@ -1,6 +1,10 @@
+CREATE EXTENSION pg_trgm;
+
 CREATE UNLOGGED TABLE IF NOT EXISTS person (
     id uuid PRIMARY KEY,
     handle text UNIQUE NOT NULL,
     payload text NOT NULL,
-    search_vector tsvector
+    search text NOT NULL
 );
+
+CREATE INDEX ON person USING GIST (search gist_trgm_ops);
