be637c944434bdddfe4a92a31bd17cd528f68a38
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2023-08-23 01:39:17 -0300

    double test load

--- stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
+++ stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
@@ -60,17 +60,17 @@ class RinhaBackendSimulation
       constantUsersPerSec(2).during(10.seconds), // warm up
       constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
       
-      rampUsersPerSec(6).to(300).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(600).during(3.minutes) // lezzz go!!!
     ),
     buscaPessoas.inject(
       constantUsersPerSec(2).during(25.seconds), // warm up
       
-      rampUsersPerSec(6).to(50).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(100).during(3.minutes) // lezzz go!!!
     ),
     buscaInvalidaPessoas.inject(
       constantUsersPerSec(2).during(25.seconds), // warm up
       
-      rampUsersPerSec(6).to(20).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(40).during(3.minutes) // lezzz go!!!
     )
   ).protocols(httpProtocol)
 }
